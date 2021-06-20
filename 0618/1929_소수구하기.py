d4774b7334386542166db0132217c485f59ac9e2
YeongHyeon-Kim <insut69@gmail.com>
2021-06-21 01:02:08 +0900

    silver2 1929 소수구하기

---
+++ 0618/1929_소수구하기.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+M,N = map(int, input().strip().split())
+
+
+def isPrime(M,N):
+    check = [True] *(N+1)
+    check[1] = False
+    m = int(N**0.5)
+    for i in range(2,m+1):
+        if check[i]:
+            for j in range(i+i, N+1, i):
+                check[j] = False
+    return [i for i in range(M, N+1) if check[i]== True]
+
+sosu = isPrime(M,N)
+for i in range(len(sosu)):
+    print(sosu[i])
